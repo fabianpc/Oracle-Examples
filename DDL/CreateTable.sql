5f7b066c4743780c8d94b6814a20adce7939b10a
Fabian Palacios <fabianpc@gmail.com>
2018-07-18 09:38:56 -0500

    Create CreateTable.sql

---
+++ DDL/CreateTable.sql
@@ -0,0 +1,22 @@
+DECLARE
+  c INT;
+BEGIN
+  SELECT COUNT(*)
+  INTO c
+  FROM user_tables
+  WHERE table_name = 'TABLENAMEA';
+  IF c             = 1 THEN
+    EXECUTE immediate 'DROP TABLE TABLENAME_A';
+    EXECUTE immediate 'CREATE TABLE TABLENAME_A  
+    (     
+      FIELD_A VARCHAR2(10 BYTE), 
+	    FIELD_B VARCHAR2(10 BYTE)
+    )' ;
+  ELSE
+    EXECUTE immediate 'CREATE TABLE TABLENAME_A 
+    (     
+      FIELD_A VARCHAR2(10 BYTE), 
+	    FIELD_B VARCHAR2(10 BYTE)
+    )' ;
+  END IF;
+END;
